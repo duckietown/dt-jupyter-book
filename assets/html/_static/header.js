247b59c79efb7ad2817d56a5bef68e8b245daa17
Davide Iafrate <Tuxliri@users.noreply.github.com>
2023-05-15 14:37:26 +0200

    Added logo to header

--- assets/html/_static/header.js
+++ assets/html/_static/header.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
     let url_root = $("#documentation_options").data("url_root");
     let url_to_library = "{0}../".format(url_root);
-    $(".announcement.header-item").html('<a href="{0}"><i class="fa fa-solid fa-arrow-left"></i> Return to Documentation Library</a>'.format(url_to_library));
+    $(".announcement.header-item").html('<a href="{0}"><i class="fa fa-solid fa-arrow-left"></i> Return to Documentation Library</a><img src="https://www.duckietown.org/wp-content/uploads/2020/10/Duckietown-logo-small.png">'.format(url_to_library));
 });
