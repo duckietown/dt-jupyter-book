cdb0e70742138f48635f89bec18ce7e7bc296ec7
Andrea F. Daniele <afdaniele@duckietown.com>
2023-10-19 01:38:09 -0400

    DTSW-4270: added warning distribution banner at the top of the page

--- assets/html/_static/header.js
+++ assets/html/_static/header.js
@@ -2,5 +2,14 @@ $(document).ready(() => {
     let url_root = $("#documentation_options").data("url_root");
     let url_to_library = "{0}../".format(url_root);
     let logo_url = "https://github.com/duckietown/dt-jupyter-book/blob/08319d57a8cd413653656268e76b9dadc72333e1/assets/dtlogo.png?raw=true"
-    $(".announcement.header-item").html('<img src="{1}"style="object-fit:contain; width:200px;height:50px;"><a href="{0}"><i class="fa fa-solid fa-arrow-left"></i> Return to Documentation Library</a>'.format(url_to_library,logo_url));
+    let header = $(".announcement.header-item");
+    header.html(
+        header.html() +
+        ('<div class="top-bar">' +
+            '<img src="{1}"style="object-fit:contain; width:200px;height:50px;" alt="">' +
+            '<a href="{0}">' +
+                '<i class="fa fa-solid fa-arrow-left"></i> Return to Documentation Library' +
+            '</a>' +
+        '</div>').format(url_to_library,logo_url)
+    );
 });
--- packages/book_decorator/add_header.py
+++ packages/book_decorator/add_header.py
@@ -2,6 +2,17 @@ import os
 
 import yaml
 
+
+DISTRO_BANNER = {
+    # TODO: uncomment this once we switch to ente
+    # "daffy": "⚠️WARNING: You are looking at the 'daffy' version of this book. 'daffy' is now an old "
+    #          "version and we suggest you switch to the newer 'ente' distribution! ⚠️",
+    # TODO: remove this once we switch to ente
+    "ente": "⚠️WARNING: You are looking at the 'ente' version of this book. 'ente' is still under "
+            "development, we suggest you switch to the 'daffy' distribution for a more stable experience! ⚠️",
+}
+
+
 if __name__ == '__main__':
     src_path: str = os.environ.get("JB_BOOK_TMP_DIR")
 
@@ -10,8 +21,18 @@ if __name__ == '__main__':
     with open(_config_fpath, "rt") as fin:
         _config = yaml.safe_load(fin)
 
-    # add branch field
-    _config["html"]["announcement"] = "&nbsp;"
+    # use distro banner if it is defined for this distribution
+    distro: str = os.environ["LIBRARY_DISTRO"]
+    if distro in DISTRO_BANNER:
+        _config["html"]["announcement"] = DISTRO_BANNER[distro]
+
+        # format message bar if an announcement is given
+    if "announcement" in _config["html"]:
+        message: str = _config["html"]["announcement"]
+        _config["html"]["announcement"] = f'<div class="announcement-message">{message}</div>'
+    else:
+        # add empty announcement bar otherwise
+        _config["html"]["announcement"] = ""
 
     # safe _config file
     with open(_config_fpath, "wt") as fout:
