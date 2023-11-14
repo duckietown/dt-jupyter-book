a7cad29d771b8cfdb7d9f1c3159f9166c61bf33f
Andrea F. Daniele <afdaniele@duckietown.com>
2023-11-13 22:23:29 -0500

    BREAKING: moved to DTProject v4

--- launchers/default.sh
+++ launchers/default.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+source /environment.sh
+
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 
+
+# NOTE: Use the variable DT_PROJECT_PATH to know the absolute path to your code
+# NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # by default, this container builds a book
 exec dt-launcher-jb-build
