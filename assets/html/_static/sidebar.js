ac250e06bc758ef69ff07c1d6d9d427ab41744ff
Andrea F. Daniele <afdaniele@duckietown.com>
2023-12-18 12:21:43 -0500

    replaced <h1#site-title> with <p#site-title>

--- assets/html/_static/sidebar.js
+++ assets/html/_static/sidebar.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
     let book_title = $("a.dt-book-title").data("value");
     let distro = $("a.dt-library-distro").data("value");
-    let site_title = $("h1.site-logo#site-title");
+    let site_title = $("p.site-logo#site-title");
     site_title.html(`
     {0}
     <h5 class="border p-1 m-0 mt-3 w-100 rounded bg-light" style="font-size: 0.9rem">version: <code>{1}</code></h5>
