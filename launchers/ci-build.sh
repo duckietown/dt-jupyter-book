5302e60774d5a67f102761ced6f31e7c9378a037
Andrea F. Daniele <afdaniele@duckietown.com>
2024-08-31 10:19:50 -0400

    disabled PDF build on CI

--- launchers/ci-build.sh
+++ launchers/ci-build.sh
@@ -8,9 +8,16 @@ source /environment.sh
 
 set -eux
 
-# build both PDF and HTML
-mkdir -p ${JB_PDF_OUT_DIR}
+# build HTML
 mkdir -p ${JB_HTML_OUT_DIR}
+
+# NOTE: PDF build is disabled for CI builds until this is fixed:
+#  https://ci.duckietown.com/view/books%20-%20daffy/job/Book%20Build%20-%20daffy%20-%20book-opmanual-duckiebot/21/console
+#
+# build PDF
+#mkdir -p ${JB_PDF_OUT_DIR}
+
+# do build
 OPTIMIZE_IMAGES=1 dt-launcher-jb-build
 
 # store SSH_KEY to SSH_ID file
