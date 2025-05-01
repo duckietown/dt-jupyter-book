2f0d56b5273d5b1326d9de75e9cd79cd708cc188
Liam Paull <paulll@iro.umontreal.ca>
2025-05-01 19:18:27 -0400

    test

--- launchers/publish-artifacts.sh
+++ launchers/publish-artifacts.sh
@@ -18,6 +18,7 @@ chmod 600 ${SSH_ID}
 # check if we want to publish HTML
 if [ -d "${JB_HTML_OUT_DIR}" ]; then
     echo "Directory '${JB_HTML_OUT_DIR}' found. Will publish HTML."
+    cat index.html
     PUBLISH_HTML=true
 else
     echo "Directory '${JB_HTML_OUT_DIR}' not found. Skipping HTML publication."
